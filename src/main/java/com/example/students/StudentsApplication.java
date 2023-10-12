95f9b4a0ed99fb494fbed7c29e85b05763712d32
Karolina Plichta <kplichta@billtech.pl>
2023-10-12 15:58:33 +0200

    project initialization

---
+++ src/main/java/com/example/students/StudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsApplication.class, args);
+    }
+
+}
