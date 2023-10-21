85940c7275dc57d48f37d870d665097b15153fee
Karolina Plichta <kplichta@billtech.pl>
2023-10-21 22:21:07 +0200

    add comments with explanation

--- src/main/java/com/example/students/data/Student.java
+++ src/main/java/com/example/students/data/Student.java
@@ -11,13 +11,13 @@ import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Getter
-@Entity
+@Entity // ta adnotacja informuje nas oraz spring boota o tym, że jest to Javovy model tabeli w bazie ( w tym wypadku tabeli student) i na ten obiekt powinno zostać wykonane mapowanie
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // bezparametrowy konstruktor jest wymagany do tworzenia tych obiektów podczas operacji na bazie, ustawnienia access na protected zapewnia nam dostęp tego konstruktora dla hibernate, a jednocześnie zamyka ten konstruktor na dostęp z zewnątrz
 public class Student {
 
-    @Id
-    @GeneratedValue
+    @Id //jak tabela w bazie tak samo każda encja musi mieć id
+    @GeneratedValue //ta adnotacja informuje nas o tym, że wartość pola id będzie generowana automatycznie i pole to nie powinno być uzupełniane przez apliakcje przy tworzeniu obiektu
     private UUID id;
     private String name;
     private StudentUnit unit;
--- src/main/java/com/example/students/data/StudentRepository.java
+++ src/main/java/com/example/students/data/StudentRepository.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
+@Repository //ta adnotacja mówi springowi, że jest to definicja beana z warstwy JPA (warstwa będzie miała znaczenie przy testach)
+//rozszerzenie tego interface o CrudRepository pozwala nam nie tworzyć jego implementacji - możemy zostawić to springowi, który na podstawie nazw metod oraz odpowienich adnotacji wygeneruje za nas implementacje
+//możemy dodatkowo skorzystać z interface'ów PagingAndSortingRepository lub JpaRepository, jesli potrzebujemy dodatkowych funkcji udostępnianych przez te interface'y
 public interface StudentRepository extends CrudRepository<Student, UUID> {
 
-    @Modifying
-    @Transactional
+    @Modifying // dodajemy zawsze gdy zapytanie modyfikuje w jakiś sposób dane - w przypadku springa będą to query z delete lub update, choć sam update częściej realizowany jest przez obiekty z gotową metodą save (z interface CrudRepository), tak jak insert
+    @Transactional // tą adnotacją otwieramy transakcje na bazie danych - mała uwaga nie musi ona znajdować się w repozytorium, może znajdować się w dowolnym miejscu w kodzie
+    //znacznie częściej stosuje się ją w serwisach aby uniknąć otwierania wielu małych transakcji
     void deleteByName(String name);
 
-    @Query("select max(s.index) from Student s group by s.id")
+    @Query("select max(s.index) from Student s group by s.id") //query pozwala napisać nam bardziej skomplikowane zapytania, których spring nie jest w stanie wygenerować z metod
     Long findMaxIndex();
 }
